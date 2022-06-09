dfe5d31027469d66a1cf2a2634e9cd9caa5abad6
nb-midwestern <nb@buildmidwestern.com>
2022-06-08 18:07:52 -0600

    road trip commits

--- app/routes/reminder.tsx
+++ app/routes/reminder.tsx
@@ -3,6 +3,7 @@
 import {
   ActionFunction,
   json,
+  LinksFunction,
   LoaderFunction,
   redirect,
 } from "@remix-run/node";
@@ -12,15 +13,21 @@ import { FormField } from "~/components/FormField/FormField";
 import { Layout } from "~/components/Layout";
 import { addReminder } from "~/utils/auth.reminder.server";
 import { getUser, requireUserId } from "~/utils/auth.server";
-import { backgroundColorMap } from "~/utils/constants";
+import { backgroundColorMap, colorMap } from "~/utils/constants";
 import { IReminder } from "~/utils/types.server";
 import { validateName } from "~/utils/validators.server";
 import Pusher from "pusher-js";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import styles from "../styles/reactDatePicker.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: styles }];
+};
 
 export const loader: LoaderFunction = async ({ request }) => {
   const appkey = process.env.PUSHER_APP_KEY;
   const cluster = process.env.PUSHER_CLUSTER;
-
   return json({ cluster });
 };
 
@@ -48,6 +55,8 @@ export const action: ActionFunction = async ({ request }) => {
   const title = form.get("title");
   const description = form.get("description");
   const dueDate = form.get("dueDate");
+  const time = form.get("reminderTime");
+  const frequency = form.get("frequency");
   const sendReminderAt = form.get("sendReminderAt");
   const repeatFreq = form.get("repeatFreq");
   const priority = form.get("priority");
@@ -55,10 +64,12 @@ export const action: ActionFunction = async ({ request }) => {
   if (
     typeof action !== "string" ||
     typeof title !== "string" ||
-    typeof description !== "string"
-    // typeof dueDate !== "string" ||
+    typeof description !== "string" ||
+    typeof dueDate !== "string" ||
+    typeof time !== "string" ||
+    typeof frequency !== "number" ||
     // typeof sendReminderAt !== "string" ||
-    // typeof repeatFreq !== "string" ||
+    typeof repeatFreq !== "string"
     // typeof priority !== "string"
   ) {
     console.log("Invalid form data");
@@ -102,46 +113,112 @@ export const action: ActionFunction = async ({ request }) => {
 export default function Reminder() {
   const actionData = useActionData();
   const [errors, setErrors] = useState(actionData?.errors || {});
+  const [dueDate, setDueDate] = useState("");
   const [formData, setFormData] = useState({
     title: actionData?.fields?.title || "",
     description: actionData?.fields?.description || "",
+    dueDate: actionData?.fields?.dueDate || "",
+    time: actionData?.fields?.time || "",
+    frequency: actionData?.fields?.time || 0,
   });
 
-  useEffect(() => {}, [actionData]);
+  useEffect(() => { }, [actionData]);
 
   const handleInputChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
+    event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>,
     field: string
   ) => {
+    console.log(event.target.value)
     setFormData((form) => ({ ...form, [field]: event.target.value }));
+  };
+
+  const isPresent = (date: Date) => {
+    if (date >= new Date()) return date;
   };
   return (
     <Layout>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/react-datepicker/2.14.1/react-datepicker.min.css"
+      />
+
       <div
         className={`h-full justify-center items-center flex flex-col gap-y-4 ${backgroundColorMap.SECONDARY_GRADIANT}`}
       >
-        <div className="rounded-md bg-gray-200 p-6 w-96">
-          <h1 className="text-5xl font-extrabold text-slate-900">Reminder</h1>
+        <div className="rounded-md bg-gray-200 p-6 w-200">
+          <h1 className="text-5xl font-extrabold text-slate-900">
+            Add Reminder
+          </h1>
           <form method="POST">
             <FormField
               htmlFor="title"
-              label="title"
+              label="Title"
               value={formData.title}
               onChange={(e) => handleInputChange(e, "title")}
               error={errors?.title}
             />
             <FormField
               htmlFor="description"
-              label="description"
+              label="Description"
               value={formData.description}
               onChange={(e) => handleInputChange(e, "description")}
               error={errors?.description}
             />
+            <div
+              className=""
+            >
+              <label className={`font-semibold ${colorMap.PRIMARY_DARK}`}>
+                Due Date
+              </label>
+              <DatePicker
+                className="w-full p-2 rounded-md my-2 hover:shadow-lg focus:shadow-lg  transition duration-300 ease-in-out hover:-translate-y-1 focus:-translate-y-1"
+                selected={formData.dueDate}
+                onChange={(e: Date) => handleInputChange(e.toISOString, "dueDate")}
+                todayButton="Today"
+                filterDate={isPresent}
+              />
+            </div>
+
+            <label className={`font-semibold ${colorMap.PRIMARY_DARK}`}>
+              Repeat Frequency
+            </label>
+            <input
+              value={formData.time}
+              onChange={(e) => handleInputChange(e, "reminderTime")}
+              type="time"
+              id="reminderTime"
+              name="reminderTime"
+              min="00:00"
+              max="12:00"
+              required
+              className="w-full p-2 rounded-md my-2 hover:shadow-lg focus:shadow-lg  transition duration-300 ease-in-out hover:-translate-y-1 focus:-translate-y-1"
+            />
+
+            <label className={`font-semibold ${colorMap.PRIMARY_DARK}`}>
+              Repeat Frequency
+            </label>
+            <select
+              name="frequency"
+              className="w-full p-2 rounded-md my-2 hover:shadow-lg focus:shadow-lg  transition duration-300 ease-in-out hover:-translate-y-1 focus:-translate-y-1"
+              value={formData.frequency}
+              onChange={(e) => handleInputChange(e, "frequency")}
+            >
+              <option value={0}>
+                None
+              </option>
+              <option value={10}>Daily</option>
+              <option value={12}>Week Days</option>
+              <option value={24}>Weekends</option>
+              <option value={20}>Weekly</option>
+              <option value={30}>Monthly</option>
+              <option value={40}>Yearly</option>
+            </select>
+
             <button
               type="submit"
               name="_action"
               value={"reminder"}
-              className="rounded-xl bg-yellow-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
+              className="rounded-md bg-teal-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
             >
               set new reminder
             </button>
