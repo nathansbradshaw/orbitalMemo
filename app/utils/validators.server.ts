49efad0e41e3267a494d8a26e5cc88140987cc5f
nb-midwestern <nb@buildmidwestern.com>
2022-06-14 22:22:07 -0600

    implement most of the reminder creation form

--- app/utils/validators.server.ts
+++ app/utils/validators.server.ts
@@ -1,19 +1,19 @@
 // app/utils/validators.server.ts
 
-export const validateEmail = (email: string):string | undefined => {
+export const validateEmail = (email: string): string | undefined => {
     var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9-]+)*$/;
     if (!email.length || !validRegex.test(email)) {
         return 'Invalid email address';
     }
 }
 
-export const validatePassword = (password: string):string | undefined => {
+export const validatePassword = (password: string): string | undefined => {
     if (password.length < 5) {
         return 'Please enter a password with at least 5 characters';
     }
 }
 
-export const validateName = (name: string):string | undefined => {
+export const validateName = (name: string): string | undefined => {
     if (!name.length) {
         return 'Please enter a value';
     }
